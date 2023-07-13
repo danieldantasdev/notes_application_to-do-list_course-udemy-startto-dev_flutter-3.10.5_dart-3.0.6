562ac81f0f626633f63f1bf3c6b8f8360c511445
danieldantasdev <contatolobinhodev@gmail.com>
2023-07-12 21:04:44 -0300

    feat: add verification in method convert date in minutes, hour and day

--- lib/utils/methods_global.util.dart
+++ lib/utils/methods_global.util.dart
@@ -1,7 +1,9 @@
 class MethodsGlobal {
   String convertDate(DateTime dateTime) {
-    return '${dateTime.toLocal().day}/'
+    return '${dateTime.toLocal().day < 10 ? ('0${dateTime.toLocal().day}') : dateTime.toLocal().day}/'
         '${dateTime.toLocal().month < 10 ? ('0${dateTime.toLocal().month}') : dateTime.toLocal().month}/'
-        '${dateTime.toLocal().year} - ${dateTime.toLocal().hour}:${dateTime.toLocal().minute}';
+        '${dateTime.toLocal().year} - '
+        '${dateTime.toLocal().hour < 10 ? ('0${dateTime.toLocal().hour}') : dateTime.toLocal().hour}:'
+        '${dateTime.toLocal().minute < 10 ? ('0${dateTime.toLocal().minute}') : dateTime.toLocal().minute}';
   }
 }
